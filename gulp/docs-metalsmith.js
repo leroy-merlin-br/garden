c0516a65ed3c1969d96a5c5abe2efea6ba9234ef
Leandro Grillo <leandro.cgrillo@gmail.com>
2015-12-29 19:20:50 -0200

    refactor(metalsmith): add metalsmith cli
    We were having some problems trying to use a code highlight, so we
    decide to remove metalsmith.json and use cli. Add prism as code
    highlight and an workaround in metalsmith to make prism work properly.
    
    Closes: #26

--- gulp/docs-metalsmith.js
+++ gulp/docs-metalsmith.js
@@ -3,7 +3,7 @@
 import {exec} from 'child_process';
 
 export default (done) => {
-  return exec('./node_modules/.bin/metalsmith', (err) => {
+  return exec('node metalsmith', (err) => {
     if (err) {
       console.log(err);
     }
