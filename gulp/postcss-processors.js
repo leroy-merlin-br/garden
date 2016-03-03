458e6337d315b48fff2cd7d05d735b5b7fa24bba
Leandro Grillo <leandro.cgrillo@gmail.com>
2016-03-03 12:16:10 -0300

    docs(grid): add mixins do docs css

--- gulp/postcss-processors.js
+++ gulp/postcss-processors.js
@@ -9,7 +9,6 @@ import postcssCustomMedia from 'postcss-custom-media';
 import postcssNested from 'postcss-nested';
 import postcssMixins from 'postcss-mixins';
 import autoprefixer from 'autoprefixer';
-import lost from 'lost';
 import cssnano from 'cssnano';
 
 import defaults from '../src/css/defaults.json';
