d3521e27d4b005a2b30ebc9b1994c12e08c4e013
Will Soares <willame_soares@hotmail.com>
2017-10-09 19:06:58 -0300

    feat(tooltip): refactor jquery methods into plain JS

--- src/js/components/tooltip.js
+++ src/js/components/tooltip.js
@@ -9,9 +9,9 @@ const DEFAULTS = {
 
 class Tooltip {
   constructor (element, options = {}) {
-    this.$element = $(element)
-    this.$target = this._getTarget()
-    this.options = $.extend({}, DEFAULTS, options)
+    this.element = element
+    this.target = this._getTarget()
+    this.options = { ...DEFAULTS, ...options }
   }
 
   init () {
@@ -21,18 +21,19 @@ class Tooltip {
   }
 
   _getTarget () {
-    const targetName = this.$element.data(NAME)
-    return this.$element.find(targetName)
+    const targetName = this.element.getAttribute('data-tooltip')
+
+    return this.element.querySelectorAll(targetName)
   }
 
   _buildPopper () {
-    return new Popper(this.$element[0], this.$target[0], this.options)
+    return new Popper(this.element, this.target[0], this.options)
   }
 
   _registerPopper () {
     const popper = this._buildPopper()
 
-    this.$element.data('popper', popper)
+    $(this.element).data('popper', popper)
   }
 }
 
