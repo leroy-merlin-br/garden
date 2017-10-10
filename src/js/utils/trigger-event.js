54f7a55325f2c45241c689935b4d459d995827a1
Will Soares <willame_soares@hotmail.com>
2017-10-10 19:07:08 -0300

    feat(trigger-event): refactor util

--- src/js/utils/trigger-event.js
+++ src/js/utils/trigger-event.js
@@ -1,13 +1,17 @@
 export default (el, type) => {
+  let e
+
   if ('createEvent' in document) {
     // modern browsers, IE9+
-    const e = document.createEvent('HTMLEvents')
+    e = document.createEvent('HTMLEvents')
     e.initEvent(type, false, true)
-    el.dispatchEvent(e)
-  } else {
-    // IE 8
-    const e = document.createEventObject()
-    e.eventType = type
-    el.fireEvent(`on${e.eventType}`, e)
+
+    return el.dispatchEvent(e)
   }
+
+  // IE 8
+  e = document.createEventObject()
+  e.eventType = type
+
+  return el.fireEvent(`on${e.eventType}`, e)
 }
