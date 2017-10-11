16ef20b755aab477053eda98a4428510674e7e15
leandrogrillo <hi@leandrogrillo.io>
2017-10-11 09:04:38 -0300

    feat(trigger-event): remove if to ie8 fallback

--- src/js/utils/trigger-event.js
+++ src/js/utils/trigger-event.js
@@ -1,17 +1,6 @@
-export default (el, type) => {
-  let e
+export default (element, type) => {
+  const event = document.createEvent('HTMLEvents')
+  event.initEvent(type, false, true)
 
-  if ('createEvent' in document) {
-    // modern browsers, IE9+
-    e = document.createEvent('HTMLEvents')
-    e.initEvent(type, false, true)
-
-    return el.dispatchEvent(e)
-  }
-
-  // IE 8
-  e = document.createEventObject()
-  e.eventType = type
-
-  return el.fireEvent(`on${e.eventType}`, e)
+  return element.dispatchEvent(event)
 }
