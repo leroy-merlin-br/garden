8243ef16e24985ee79811251cdf6067f420d08dd
leandrogrillo <hi@leandrogrillo.io>
2017-10-19 15:53:57 -0200

    feat(tooltip): add specs and store popper instance without jquery

--- test/components/tooltip.spec.js
+++ test/components/tooltip.spec.js
@@ -23,24 +23,39 @@ describe('Tooltip spec', () => {
       expect(instance.init()).to.be.equal(instance)
     })
 
-    it('should register the popper instance on the element', () => {
+    it('should call instance.registerComponent', sinon.test(function () {
+      const stub = this.stub(instance, 'registerComponent')
+
       instance.init()
 
-      expect($(instance.element).data('popper')).to.be.an.instanceof(Popper)
-    })
+      expect(stub.calledOnce).to.be.true
+    }))
   })
 
-  describe('_getTarget', () => {
+  describe('getTarget', () => {
     it('should return the target element described in data-tooltip', () => {
-      const result = instance._getTarget()
+      const result = instance.getTarget()
       expect(result[0].innerText).to.have.string('Sample tooltip')
     })
   })
 
-  describe('_buildPopper', () => {
+  describe('buildPopper', () => {
     it('should create and return a new Popper instance', () => {
-      const result = instance._buildPopper()
-      expect(result).to.be.an.instanceof(Popper)
+      const popperInstance = instance.buildPopper()
+
+      expect(popperInstance).to.be.an.instanceof(Popper)
+    })
+  })
+
+  describe('registerComponent', () => {
+    it('should save popper instance in instance.element.attributes', () => {
+      instance
+        .init()
+        .registerComponent()
+
+      const { component } = instance.element.attributes
+
+      expect(component).to.be.an.instanceof(Popper)
     })
   })
 })
