7c0cbe20b9fe05d1382f0b9ddfbfe0594e01f5c1
Will Soares <willame_soares@hotmail.com>
2017-11-06 19:20:09 -0300

    refactor(scroll): add docblocks to component implementation

--- src/js/components/scroll.js
+++ src/js/components/scroll.js
@@ -7,20 +7,35 @@ const DEFAULTS = {
 }
 
 class Scroll {
+  /**
+   * Instantiate a new Scroll component.
+   *
+   * @param {Element} element Element to be used as the scrolling reference.
+   * @param {Object}  options Options used to customize the component.
+   */
   constructor (element, options) {
     this.element = element
     this.options = { ...DEFAULTS, ...options }
   }
 
+  /**
+  * Initialize the component and automatically perform the scrolling action.
+  */
   init () {
     this.scrollToElement()
     this.registerComponent()
   }
 
+  /**
+  * Scroll page to the specified element position.
+  */
   scrollToElement () {
     this.scroll = jump(this.element, this.options)
   }
 
+  /**
+  * Add Scroll instance to the element attributes object.
+  */
   registerComponent () {
     this.element.attributes.component = new Scroll(this.element, this.options)
   }
