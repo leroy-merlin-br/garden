d006911b019142d5fa1791359e005c2b9057b876
Fabricio Augusto <fabricionaweb@gmail.com>
2017-11-10 15:40:25 -0200

    refactor(validation): remove jquery from required and form

--- src/js/components/validation/required.js
+++ src/js/components/validation/required.js
@@ -1,13 +1,15 @@
 /**
  * Check if a field of type checkbox or radio is checked.
  */
-export default (field, $form) => {
+export default (field, form) => {
   if (field.type === 'checkbox') {
     return field.checked
   }
 
   if (field.type === 'radio') {
-    return $form.find(`[name="${field.name}"]:checked`).length
+    const radio = form.querySelectorAll(`[name="${field.name}"]:checked`)
+
+    return !!radio.length
   }
 
   return !!field.value.trim()
--- test/components/validation/required.spec.js
+++ test/components/validation/required.spec.js
@@ -1,64 +1,56 @@
 import required from '../../../src/js/components/validation/required'
 
 describe('Validation@required', () => {
+  let form, field
+
+  before(() => {
+    fixture.setBase('test/fixture')
+  })
+
+  beforeEach(() => {
+    form = fixture.load('validation/required.html')[0]
+    field = form.querySelector('input')
+  })
+
+  afterEach(() => {
+    fixture.cleanup()
+  })
+
   it('should return true if the field has value', () => {
-    let field = {
-      value: 'foo'
-    }
+    field.value = 'foo'
 
     expect(required(field)).to.be.true
   })
 
   it('should return false if the field does not have value', () => {
-    let field = {
-      value: ''
-    }
+    field.value = ''
 
     expect(required(field)).to.be.false
   })
 
   it('should return true if the field is a checkbox and is checked', () => {
-    let field = {
-      type: 'checkbox',
-      checked: true
-    }
+    field.type = 'checkbox'
+    field.checked = true
 
     expect(required(field)).to.be.true
   })
 
   it('should return false if the field is a checkbox and is not checked', () => {
-    let field = {
-      type: 'checkbox'
-    }
+    field.type = 'checkbox'
 
-    expect(required(field)).to.be.falsy
+    expect(required(field)).to.be.false
   })
 
   it('should return true if the field is a radio and it is selected', () => {
-    let field = {
-      type: 'radio'
-    }
-
-    let form = {
-      find: () => ({
-        length: true
-      })
-    }
+    field.type = 'radio'
+    field.checked = true
 
     expect(required(field, form)).to.be.true
   })
 
   it('should return false if the field is a radio and there is no radio selected', () => {
-    let field = {
-      type: 'radio'
-    }
+    field.type = 'radio'
 
-    let form = {
-      find: () => ({
-        length: 0
-      })
-    }
-
-    expect(required(field, form)).to.be.falsy
+    expect(required(field, form)).to.be.false
   })
 })
